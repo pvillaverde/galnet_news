6d000f145b825a3faab86ab91a282c7d957b9e01
pvillaverde <pvillaverde.git@gmail.com>
2024-07-12 23:33:39 +0200

    🔧 Change default values

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ interface GalnetNew {
 }
 // Setup Variables
 const WEBHOOK_URL = new URL(Deno.env.get("WEBHOOK_URL") as string)
-const LANGUAGE = Deno.env.get("LANGUAGE") || "es-ES";
-const saveFile = Deno.env.get("SAVE_PATH") || "./last_record.txt";
+const LANGUAGE = Deno.env.get("LANGUAGE") || "en-GB";
+const saveFile = Deno.env.get("SAVE_PATH") || "/data/last_record.txt";
 const API_URL = `https://cms.zaonce.net/${LANGUAGE}/jsonapi/node/galnet_article?&sort=-published_at&page[offset]=0&page[limit]=1`
 
 const logger = new Logger({
