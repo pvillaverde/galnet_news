9b5b8fe376996eca9f23fcace768fb1a46caeade
pvillaverde <pvillaverde.git@gmail.com>
2024-08-07 13:50:59 +0200

    🐛 Fix news not publishing when length > 2000 characters

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ interface GalnetNew {
    published_at: number;
 }
 // Setup Variables
-const WEBHOOK_URL = new URL(Deno.env.get("WEBHOOK_URL") as string)
+const WEBHOOK_URL = new URL(Deno.env.get("WEBHOOK_URL") as string,)
 const LANGUAGE = Deno.env.get("LANGUAGE") || "en-GB";
 const saveFile = Deno.env.get("SAVE_PATH") || "/data/last_record.txt";
 const API_URL = `https://cms.zaonce.net/${LANGUAGE}/jsonapi/node/galnet_article?&sort=-published_at&page[offset]=0&page[limit]=1`
@@ -47,17 +47,58 @@ async function fetchFromApi(): Promise<GalnetNew | null> {
    }
 }
 
-async function main() {
-   const storedId = await readfromFile(saveFile);
-   const latestNew = await fetchFromApi();
-
-   if (latestNew && storedId !== latestNew.id) {
-      logger.info(`ID has changed from ${storedId} to ${latestNew.id}. Updating...`);
-      const webhook = new Webhook(WEBHOOK_URL);
-      await webhook.send(latestNew.message);
-      await Deno.writeTextFile(saveFile, latestNew.id);
+// Function extracted from discord.js
+function splitMessage(text: any, { maxLength = 2000, char = '\n', prepend = '', append = '' } = {}) {
+   if (text.length <= maxLength) return [text];
+   let splitText = [text];
+   if (Array.isArray(char)) {
+      while (char.length > 0 && splitText.some(elem => elem.length > maxLength)) {
+         const currentChar = char.shift();
+         if (currentChar instanceof RegExp) {
+            splitText = splitText.flatMap(chunk => chunk.match(currentChar));
+         } else {
+            splitText = splitText.flatMap(chunk => chunk.split(currentChar));
+         }
+      }
    } else {
-      logger.info(`ID has not changed. Current ID is ${storedId}.`);
+      splitText = text.split(char);
+   }
+   if (splitText.some(elem => elem.length > maxLength)) throw new RangeError('SPLIT_MAX_LEN');
+   const messages = [];
+   let msg = '';
+   for (const chunk of splitText) {
+      if (msg && (msg + char + chunk + append).length > maxLength) {
+         messages.push(msg + append);
+         msg = prepend;
+      }
+      msg += (msg && msg !== prepend ? char : '') + chunk;
+   }
+   return messages.concat(msg).filter(m => m);
+}
+
+async function main() {
+   try {
+      const storedId = await readfromFile(saveFile);
+      const latestNew = await fetchFromApi();
+
+      if (latestNew && storedId !== latestNew.id) {
+         logger.info(`ID has changed from ${storedId} to ${latestNew.id}. Updating...`);
+         const webhook = new Webhook(WEBHOOK_URL);
+         logger.info(latestNew.message);
+         const messageChunks = splitMessage(latestNew.message, {
+            maxLength: 2000,
+            prepend: '>>> ',
+            char: '\n'
+         });
+         messageChunks.forEach(async chunk => {
+            await webhook.send(chunk);
+         });
+         //await Deno.writeTextFile(saveFile, latestNew.id);
+      } else {
+         logger.info(`ID has not changed. Current ID is ${storedId}.`);
+      }
+   } catch (error) {
+      logger.error(error)
    }
 }
 
